040106e6cffa07f90608a3dfb464c261f88bfc86
carlkrok <carlmk@stud.ntnu.no>
2018-04-29 13:31:54 +0200

    Update main_balanced.py

--- main_balanced.py
+++ main_balanced.py
@@ -15,15 +15,15 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    #print("Creating model...")
-    #our_own_cnn_3.our_own_cnn_3()
+    print("Creating model...")
+    our_own_cnn_4.our_own_cnn_4()
 
     print("Loading model...")
-    #model = load_model("our_own_cnn_3")
-    model = load_model("saturday_best_model.h5")
+    model = load_model("our_own_cnn_4")
+    #model = load_model("saturday_best_model.h5")
     model.summary()
 
-    csv_logger = CSVLogger('log_friday.csv', append=True, separator=';')
+    csv_logger = CSVLogger('log_sunday.csv', append=True, separator=';')
     checkpoint = ModelCheckpoint('curr_best_model.h5', monitor='val_loss',verbose=0,save_best_only=True, mode='auto') #Saved_models
 
     np_images_train = np.load("np_images_balanced.npy")
@@ -35,7 +35,7 @@ def main():
     history = model.fit(x=np_images_train, y=np_steering_train, epochs=200, batch_size=10, callbacks=[checkpoint, csv_logger], validation_data=(np_val_images_train, np_val_steering_train)) 
 
     print("Saving the model...")
-    model.save("our_own_cnn_3_trained_sunday.h5")
+    model.save("our_own_cnn_4_trained_sunday.h5")
 
 
     print("Finished!")
